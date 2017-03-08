83460e05ca5898cf00688fa75d0860145acb0e81
judy6886 <samantha_judy@163.com>
2017-03-09 00:14:42 +0800

    完善留言板

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@
     //新建模块
     var app=angular.module("CommentApp",[]);
     //定义常量
-    app.constant("commentAPI","http://127.0.0.1:32768/api/comments");
+    app.constant("commentAPI","http://127.0.0.1:32768/api/comments/");
 })();
